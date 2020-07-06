dbec00957b5b7cb38b95e7d4ac6fd82349433818
Merkado13 <marketyt@live.com>
2020-07-06 14:32:24 +0200

    Keys Movement

---
+++ TwoKeys/Assets/Scripts/KeyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode key;
+
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            transform.Translate(transform.forward * 1, Space.World);
+        }
+        else if (Input.GetKeyUp(key))
+        {
+            transform.Translate(-transform.forward * 1, Space.World);
+
+        }
+    }
+}
